072336657200fa6e496fead08dcee172e38d8b19
takahiro hagiuda <takahiro.hagiuda@gmail.com>
2024-01-16 18:05:43 +0900

    シェルの枠組みを実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        // プロンプトを表示
+        print!("> ");
+
+        // 標準入力から文字列を読み取る
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("faild to read line");
+
+        println!("You enterd: {}", input);
+
+        // 空白で区切り、ベクタに格納
+        let tokens: Vec<&str> = input.split_whitespace().collect();
+
+        // コマンドと引数を分ける
+        let command = tokens[0];
+        let args = tokens[1..].to_vec();
+
+        println!("コマンド: {}", command);
+        println!("引数: {:?}", args);
+    }
 }
